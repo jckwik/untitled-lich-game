129dcf5f6f872356ea552f71f199ae9e0549f07c
Sushi <eez4347@gmail.com>
2020-04-17 17:26:47 -0400

    Add dependancies

---
+++ UntitledLichGame/app.jsx
@@ -0,0 +1,48 @@
+﻿//declare var require: any
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//export class Hello extends React.Component {
+//	render() {
+//		return (
+//			<h1>Welcome to React! =)</h1>
+//		);
+//	}
+//}
+
+//ReactDOM.render(<Hello />, document.getElementById('root'));
+
+function Button(props) {
+	const handleClick = () => props.onClickFunction(props.increment);
+	return (
+		<button onClick={handleClick}>
+			+{props.increment}
+		</button>
+	);
+}
+
+function Display(props) {
+	return (
+		<div>{props.message}</div>
+	);
+}
+
+function App() {
+	const [counter, setCounter] = React.useState(0);
+	const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
+	return (
+		<div>
+			<Button onClickFunction={incrementCounter} increment={1} />
+			<Button onClickFunction={incrementCounter} increment={5} />
+			<Button onClickFunction={incrementCounter} increment={10} />
+			<Button onClickFunction={incrementCounter} increment={100} />
+			<Display message={counter} />
+		</div>
+	);
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root'),
+);
---
+++ UntitledLichGame/webpack.config.js
@@ -0,0 +1,28 @@
+﻿/// <binding />
+module.exports = {
+	devtool: 'source-map',
+	entry: "./app.jsx",
+	mode: "development",
+	output: {
+		filename: "./app-bundle.js"
+	},
+	resolve: {
+		extensions: ['.Webpack.js', '.web.js', '.ts', '.js', '.jsx', '.tsx']
+	},
+	//devServer: {
+	//	contentBase: ".",
+	//	host: "localhost",
+	//	port: 4242
+	//},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader'
+				}
+			}
+		]
+	}
+}

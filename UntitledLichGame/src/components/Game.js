6b815fb48ac23f88de78d108eea008dd1dc9a054
jckwik <jckwikspam@gmail.com>
2020-04-18 22:11:37 -0400

    Add Bootstrap components. Add resource and building framing

--- UntitledLichGame/src/components/Game.js
+++ UntitledLichGame/src/components/Game.js
@@ -2,16 +2,55 @@
 import Button from './Button';
 import Display from './Display';
 
+class Resource {
+    constructor(name, quantity) {
+        this.name = name;
+        this.quantity = quantity;
+    }
+}
+
+class Building {
+    constructor(quantity, workersAssigned, powerRequired, effect) {
+        this.quantity = quantity;
+        this.workersAssigned = workersAssigned;
+        this.powerRequired = powerRequired;
+        this.effect = effect;
+    }
+}
+
+const resources = {
+    "Worker": 1,
+    "Worker Power": 1,
+    "Bone": 1,
+    "Wood": 0
+};
+
+const buildings = [
+    { graveyard: new Building(0, 0, 0, () => AddResource(resources.bone, 1 * this.quantity)) }
+];
+
+function AddResource(resource, amount) {
+    resource.quantity = resource.quantity + amount;
+}
+
+function BuildResourceDisplay() {
+    var output = [];
+    for (const [resourceKey, resourceQuantity] of Object.entries(resources)) {
+        console.log(resourceKey, resourceQuantity);
+        output.push(<Display resource={resourceKey} quantity={resourceQuantity} />);
+    }
+    return (<ul key="blah">
+        {output}
+        </ul>);
+}
+
 export default function Game() {
-	const [counter, setCounter] = React.useState(0);
-	const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
+    const [counter, setCounter] = React.useState(0);
+    const incrementCounter = (incrementValue) => setCounter(counter + incrementValue);
+    const resourceDisplayGroup = BuildResourceDisplay();
 	return (
 		<div>
-			<Button onClickFunction={incrementCounter} increment={1} />
-			<Button onClickFunction={incrementCounter} increment={5} />
-			<Button onClickFunction={incrementCounter} increment={10} />
-			<Button onClickFunction={incrementCounter} increment={100} />
-			<Display message={counter} />
+            {resourceDisplayGroup}
 		</div>
 	);
 }
--- UntitledLichGame/src/index.js
+++ UntitledLichGame/src/index.js
@@ -1,7 +1,8 @@
 ﻿import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Game from './components/Game';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
 
 ReactDOM.hydrate(
     <Game />,
